dea90d2a761ce57a748ec92c140140deb5eccedd
Henrique Alves <henriquelalves@gmail.com>
2020-05-24 12:30:02 -0300

    Fix draft button

--- scripts/ui/DraftButton.gd
+++ scripts/ui/DraftButton.gd
@@ -8,14 +8,16 @@ export(String) var button_text setget set_button_text
 func set_button_text(value : String):
 	button_text = value
 	$ShortLabel.text = value
-	$LongLabel.text = value
 	
 	_refresh_label()
 
 func _refresh_label():
-	var visible_short = $ShortLabel.get("custom_fonts/font").get_string_size(button_text).x < rect_size.x - 60
-	$ShortLabel.visible = visible_short
-	$LongLabel.visible = not visible_short
+	var font : Font = $ShortLabel.get("custom_fonts/font")
+	var max_width = $ControlLabelSize.rect_size.x
+	font.set("size", 50)
+	while (font.get_string_size(button_text).x > max_width &&\
+	font.get("size") > 26):
+		font.set("size", font.get("size") - 2)
 
 func _on_resized():
 	rect_pivot_offset = rect_size/2
